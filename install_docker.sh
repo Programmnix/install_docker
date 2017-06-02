83b9e8041cfe25d283416ba291b9c301b5d06129
Programmnix <dominik@klotz-home.de>
2017-06-03 00:37:30 +0200

    Update install_docker.sh
    Add sudo rigths to docker compose

--- install_docker.sh
+++ install_docker.sh
@@ -19,8 +19,8 @@ sudo groupadd docker
 
 sudo usermod -aG docker $USER
 
-curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 exit 0
